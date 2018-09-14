937933d4fbc465558a8f51c90193fc6acfe2dfd2
arod <arod1198@gmail.com>
2018-09-13 22:39:14 -0400

    prepared the payload for the indico api

--- RedditBot/indico.py
+++ RedditBot/indico.py
@@ -1,4 +1,9 @@
 import indicoio
 from config import INDICO_API_KEY
-
+from redditbot import payload
 indicoio.config.api_key = INDICO_API_KEY
+
+# single example
+print(payload)
+results = indicoio.sentiment(payload)
+print(results)
--- RedditBot/redditbot.py
+++ RedditBot/redditbot.py
@@ -5,9 +5,15 @@ from nba_players import players_dic, start_time
 
 reddit = praw.Reddit(client_id='fFCCdbz1412TtQ', client_secret='KkSdtRChP6uq-fe2usK04GYttbk', username='Arod16', password='paugasol16', user_agent='testscript by /u/Arod15')
 
+payload = []
 def clear():
     os.system('cls')
-
+def prepare_payload():
+    for player in players_dic:
+        if players_dic[player]['name-drops'] != 0:
+            for comment in players_dic[player]['comments']:
+                payload.append(comment)
+    print(payload)
 def check_nonzero_values():
     for name in players_dic:
         if players_dic[name][0] != 0:
@@ -19,38 +25,30 @@ def check_nonzero_values():
                 comment = reddit.comment(comment_id)
                 print('Comment: ' + comment.body)
                 print('Comment Score: ' + str(comment.score))
-def check_for_player(comment_string, comment_id):
-    # Note: tried to get creative and check if a substring is a name
-    # for word in comment_string.split():
-    #     for key in players_dic:
-    #         if word in key.split():
-    #             players_dic[key] += 1
+def check_for_player(comment_string):
     for name in players_dic:
         # going to start with just full name substrings like 'Kyrie Irving'
         if name.lower() in comment_string.lower():
-            players_dic[name][0] += 1
-            players_dic[name].append(comment_id)
+            players_dic[name]['name-drops'] += 1
+            players_dic[name]['comments'].append(comment_string)
 
 def search_for_players_frontpage():
-    i = 0
     for submission in reddit.subreddit('NBA').hot(limit=20):
         submission.comments.replace_more(limit=0)
         flat_comments = submission.comments.list()
         for comment in flat_comments:
-            comment_id = comment.fullname.split('_')[1]
-            check_for_player(comment.body, comment_id)
-        i += 1
-    print(i)
+            # comment_id = comment.fullname.split('_')[1]
+            check_for_player(comment.body)
     # check_nonzero_values()
 
 def search_for_players_new():
     for comment in reddit.subreddit('NBA').stream.comments():
         comment_id = comment.fullname.split('_')[1]
-        check_for_player(comment.body, comment_id)
-        print(comment.body)
+        check_for_player(comment.body)
 
 def main():
     search_for_players_frontpage()
+    prepare_payload()
     # action = input('Enter 0 if you want to clear the screen: ')
     # if not int(action):
     #     clear()
